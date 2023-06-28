f04d4d8c73b681a62e170d232f32506779dee774
mark <markpackham1@gmail.com>
2023-06-28 11:38:15 +0100

    add server test
    https://youtu.be/q6MxsS2iPnw?t=358

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
 
-const server = new ApolloServer({});
+const server = new ApolloServer({
+  // typeDefs
+  // resolvers
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`Server ready at port 4000`);
