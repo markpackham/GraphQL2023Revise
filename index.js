a13974f09d20a84e74067bb628cac7d0c72b1e47
mark <markpackham1@gmail.com>
2023-06-30 11:16:35 +0100

    import the database
    https://youtu.be/mjqfYgFyziU?t=214

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import db from "./_db";
 
 // types
 import { typeDefs } from "./schema.js";
 
+const resolvers = {
+  Query: {
+    games() {
+      return;
+    },
+  },
+};
+
 const server = new ApolloServer({
   typeDefs,
+  // Resolvers, how we respond to queries
 });
 
 const { url } = await startStandaloneServer(server, {
